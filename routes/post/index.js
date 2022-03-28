89c4e5d83333ebf9bee6609e3a3e15a8b67a259f
glen15 <glen910115@gmail.com>
2022-03-29 08:20:30 +0900

    update

--- routes/post/index.js
+++ routes/post/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
-        reply.code(200).send([{ title: "블로그" }, { note: "hello world" }])
+        reply.code(200).send("hi there")
     })
 }
