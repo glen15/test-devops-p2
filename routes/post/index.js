e37e6ccf2146ad0125541111e1d6775d87c05102
glen15 <glen910115@gmail.com>
2022-03-29 11:30:48 +0900

    change text

--- routes/post/index.js
+++ routes/post/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
-        reply.code(200).send("hi there")
+        reply.code(200).send("바꿔도 되는거냐")
     })
 }
