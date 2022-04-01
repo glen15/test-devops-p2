73e9643e996351090265155c8cc28cdae30cb1aa
glen15 <glen910115@gmail.com>
2022-04-01 17:44:26 +0900

    update root.js for version 11.0

--- routes/post/index.js
+++ routes/post/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
-        reply.code(200).send("git action version 10.0")
+        reply.code(200).send("git action version 11.0")
     })
 }
