7aaef7f41df14481b7defaf918efc153bbcba793
glen15 <glen910115@gmail.com>
2022-04-01 17:36:01 +0900

    git action version 10.0

--- routes/post/index.js
+++ routes/post/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
-        reply.code(200).send("git action version 8.0 without 0000")
+        reply.code(200).send("git action version 10.0")
     })
 }
