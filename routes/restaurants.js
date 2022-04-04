192cab6a7925dd5b3ffb9bb93f0f941a7d7dbc5f
glen15 <glen910115@gmail.com>
2022-04-04 16:05:13 +0900

    update 4.0

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,8 +1,8 @@
 const { getAllRestaurants, getAllReviews, updateReviews } = require("../controller/baedal.controller");
 
 async function routes(fastify, options) {
-    fastify.get("/restaurants", getAllRestaurants);
-    fastify.get("/restaurants/:id/reviews", getAllReviews);
-    fastify.put("/restaurants/:id/reviews", updateReviews);
+    fastify.get("/api/restaurants", getAllRestaurants);
+    fastify.get("/api/restaurants/:id/reviews", getAllReviews);
+    fastify.put("/api/restaurants/:id/reviews", updateReviews);
 }
 module.exports = routes;
--- routes/version/index.js
+++ routes/version/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
-        reply.code(200).send("project day 3 - git action version 3.0")
+        reply.code(200).send("project day 3 - git action version 4.0")
     })
 }
